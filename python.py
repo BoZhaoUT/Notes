99e8469fb24a1e9044b130f6da27304d8fa72673
Kenny <zb.ca@foxmail.com>
2017-09-25 23:39:08 -0400

    Add merge PDF files

--- python.py
+++ python.py
@@ -397,3 +397,17 @@ parsed = datetime.datetime.strptime("July 1, 2017", "%B %d, %Y")
 # %S - second                e.g. 00 - 59
 # %p - am or pm
 '''
+
+# merge 2 DPF files
+'''
+from PyPDF2 import PdfFileMerger
+
+pdfs = ['1.pdf', '2.pdf']
+
+merger = PdfFileMerger(strict=False)
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("result.pdf")
+'''
