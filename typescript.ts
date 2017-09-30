e2ff42ccaba26ed88174bc25b309443817dc3e24
BoZhaoUTSC <bodotzhao@gmail.com>
2017-09-30 14:45:16 -0400

    Add type conversion and arrow function

--- typescript.ts
+++ typescript.ts
@@ -1,5 +1,14 @@
 
 // equivalent to 
 // const ColorRed = 0; const ColorGreen = 1, const ColorBlue = 2;
-enum Color { Red = 0, Green = 1, Blue = 2 }
-let backgroundColor = Color.Red
+enum Color { Red = 0, Green = 1, Blue = 2 };
+let backgroundColor = Color.Red;
+
+// type conversion
+let message;
+message = 'abc';
+let endsWithC = (<string>message).endsWith('c');
+
+// lambda function or arrow function
+// equivalent to (message) => {console.log(message)}
+let doLog = (message) => console.log(message);
