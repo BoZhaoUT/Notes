d3874dcffdf259ef15a8763621413e5f71240d39
BoZhaoUTSC <bodotzhao@gmail.com>
2017-10-05 18:03:50 -0400

    Add template driven forms

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { TitleCasePipe } from './title-case.pipe';
 import { FavouriteComponent } from './favourite/favourite.component';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { LikeComponent } from './like/like.component';
     TitleCasePipe,
     FavouriteComponent,
     PanelComponent,
-    LikeComponent
+    LikeComponent,
+    InputFormatDirective,
+    ZippyComponent,
+    ContactFormComponent,
+    NewCourseFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/src/app/input-format.directive.ts
@@ -0,0 +1,28 @@
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+
+export class InputFormatDirective {
+  // note: 'appInputFormat is the name of this custom pipe
+  @Input('appInputFormat') format;
+
+  // ElementRef gives access to DOM object with this costum directive
+  constructor(private el: ElementRef) { }
+
+  // HostListen will subscribe to an event handled by target
+  // DOM object
+  @HostListener('blur') onBlur() {
+    // in this example, value will be text of input tag
+    let value: string = this.el.nativeElement.value;
+
+    if (this.format == 'lowercase')
+      this.el.nativeElement.value = value.toLocaleLowerCase();
+    else
+      this.el.nativeElement.value = value.toLocaleUpperCase();
+  }
+
+
+
+}
