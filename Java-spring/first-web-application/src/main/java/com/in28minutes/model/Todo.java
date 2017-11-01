9fc2e38bd77093b978fc322168d06cbe08ddfbdd
BoZhaoUTSC <bodotzhao@gmail.com>
2017-11-01 04:01:44 -0400

    Update java-spring

--- Java-spring/first-web-application/src/main/java/com/in28minutes/model/Todo.java
+++ Java-spring/first-web-application/src/main/java/com/in28minutes/model/Todo.java
@@ -1,71 +1,72 @@
-package com.in28minutes.todo;
+package com.in28minutes.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.Size;
 
 public class Todo {
-
 	private int id;
+
 	private String user;
+
+	@Size(min = 10, message = "Enter atleast 10 Characters.")
 	private String desc;
+
 	private Date targetDate;
 	private boolean isDone;
-	
-	
-	
-	public Todo(int id, String user, String desc, Date date, boolean isDone) {
+
+	public Todo() {
+		super();
+	}
+
+	public Todo(int id, String user, String desc, Date targetDate,
+			boolean isDone) {
 		super();
 		this.id = id;
 		this.user = user;
 		this.desc = desc;
-		this.targetDate = date;
+		this.targetDate = targetDate;
 		this.isDone = isDone;
 	}
 
 	public int getId() {
 		return id;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public String getUser() {
 		return user;
 	}
-	
+
 	public void setUser(String user) {
 		this.user = user;
 	}
-	
+
 	public String getDesc() {
 		return desc;
 	}
-	
+
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
-	
+
 	public Date getTargetDate() {
 		return targetDate;
 	}
-	
+
 	public void setTargetDate(Date targetDate) {
 		this.targetDate = targetDate;
 	}
-	
+
 	public boolean isDone() {
 		return isDone;
 	}
-	
+
 	public void setDone(boolean isDone) {
 		this.isDone = isDone;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("toString - Todo [id=%s, user=%s, desc=%s, targetDate=%s, isDone=%s]", id, user, desc, targetDate,
-				isDone);
 	}
 
 	@Override
@@ -89,6 +90,12 @@ public class Todo {
 			return false;
 		return true;
 	}
-	
-	
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Todo [id=%s, user=%s, desc=%s, targetDate=%s, isDone=%s]", id,
+				user, desc, targetDate, isDone);
+	}
+
 }
