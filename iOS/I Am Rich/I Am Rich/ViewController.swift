45b02111624b7a079c4f15f040b7d7c998c8acdb
BoZhaoUTSC <bodotzhao@gmail.com>
2018-02-21 14:18:41 -0500

    Left swipe to hide side menu

--- iOS/I Am Rich/I Am Rich/ViewController.swift
+++ iOS/I Am Rich/I Am Rich/ViewController.swift
@@ -14,53 +14,57 @@ class ViewController: UIViewController {
     // side menu
     var showSideMenu = false
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+
     
     // to hide side menu
     @IBAction func tapOutside(_ sender: Any) {
-        leadingConstraint.constant = -200
-        // animation
-        UIView.animate(withDuration: 0.3, animations: {
-            self.view.layoutIfNeeded()
-        })
+        hideSideMenu()
     }
     
+    // to hide side menu
+    @IBAction func swiftLeftExitSideMenu(_ sender: Any) {
+        hideSideMenu()
+    }
+    
+    // to hide side menu
     @IBAction func nextPage(_ sender: Any) {
-        leadingConstraint.constant = -200
+        hideSideMenu()
     }
     
     @IBAction func humbergerButton(_ sender: Any) {
         // to hide side menu
         if (showSideMenu) {
-            leadingConstraint.constant = -200
+            hideSideMenu()
         // to show side menu
         } else {
             leadingConstraint.constant = 0
-
+            toggleSideMenuanimation()
         }
-        // animation
-        UIView.animate(withDuration: 0.3, animations: {
-            self.view.layoutIfNeeded()
-        })
         showSideMenu = !showSideMenu
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func imageSwiped(_ sender: Any) {
-        print("image swiped")
-    }
-    
     @IBAction func edgePanSwiped(_ recognizer: UIScreenEdgePanGestureRecognizer) {
-        print("edge pan swiped")
         if (recognizer.state == .recognized) {
             self.humbergerButton(self)
         }
     }
     
-
+    func hideSideMenu() {
+        leadingConstraint.constant = -200
+        toggleSideMenuanimation()
+    }
+    
+    // animation
+    func toggleSideMenuanimation() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
 }
 
