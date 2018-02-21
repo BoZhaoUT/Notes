1190cfd23f7b1ff846d44707235292b10219f630
BoZhaoUTSC <bodotzhao@gmail.com>
2018-02-21 12:14:17 -0500

    Add side menu

--- iOS/I Am Rich/I Am Rich/ViewController.swift
+++ iOS/I Am Rich/I Am Rich/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    // side menu
+    var showSideMenu = false
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    // to hide side menu
+    @IBAction func tapOutside(_ sender: Any) {
+        leadingConstraint.constant = -200
+        // animation
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    @IBAction func humbergerButton(_ sender: Any) {
+        // to hide side menu
+        if (showSideMenu) {
+            leadingConstraint.constant = -200
+        // to show side menu
+        } else {
+            leadingConstraint.constant = 0
+
+        }
+        // animation
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        })
+        showSideMenu = !showSideMenu
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func imageSwiped(_ sender: Any) {
+        print("image swiped")
+    }
+    
+    @IBAction func edgePanSwiped(_ sender: Any) {
+        print("edge pan swiped")
+        self.humbergerButton(self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
