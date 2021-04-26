f1e29d0f2c6b79413898fe21dcfb7d146e9b9b24
Bo Zhao <bodotzhao@gmail.com>
2021-04-25 23:46:59 -0400

    update

--- sql.sql
+++ sql.sql
@@ -1,3 +1,14 @@
+-- print literal string
+SELECT "Hello, World";
+
+-- rename the column title to "Result", by default it's "Hello, World"
+SELECT "Hello, World" AS "Result";
+
+-- another way to rename column title
+SELECT Name, LifeExpectancy AS "Life Expantancy" FROM Country ORDER BY Name;
+
+-- the keyward "AS" was not mandatory to rename a column title
+SELECT Name, LifeExpectancy "Life Expantancy" FROM Country ORDER BY Name;
 
 -- alter is used to add, delete or modify columns in an existing table
 -- is also to add and drop various constraints on an existing table
