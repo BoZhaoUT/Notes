b25830dfe599fe730ffa8a4d40715d42d4f9bfb2
Bo Zhao <bodotzhao@gmail.com>
2021-04-26 23:34:52 -0400

    update

---
+++ scala.scala
@@ -0,0 +1,19 @@
+// data types
+// AnyVal
+// Double, Float, Int, Short, Byte
+// Char
+// Boolean
+// Unit
+
+// AnyRef
+// Object
+// Seq
+// List
+// String
+
+val num = 5             // Int
+val decimal = 5.13      // Double
+var x = 1 + 2 + 3.5     // Double, mixing double and int will get double
+List(1, 2, 3.5)         // List[Double], same as above
+List(1, true)           // List[AnyVal], mixing Int and Boolean will get AnyVal
+List(1, true, "peggy")  // List[any], mixing AnyVal and AnyRef will get Any
