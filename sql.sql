7a3db0ff8482b232894639459afc6bc7cd80cd26
Bo Zhao <bodotzhao@gmail.com>
2021-04-28 22:28:28 -0400

    update

--- sql.sql
+++ sql.sql
@@ -10,6 +10,31 @@ SELECT Name, LifeExpectancy AS "Life Expantancy" FROM Country ORDER BY Name;
 -- the keyward "AS" was not mandatory to rename a column title
 SELECT Name, LifeExpectancy "Life Expantancy" FROM Country ORDER BY Name;
 
+-- simple WHERE clause
+SELECT Name, Continent, Region FROM Country WHERE Continent = 'Europe';
+
+-- simple WHERE, ORDER BY and Limit
+SELECT Name, Continent FROM Country WHERE Continent = 'Europe' ORDER BY Name LIMIT 5;
+
+-- skip first 10
+SELECT Name, Continent FROM Country WHERE Continent = 'Europe' ORDER BY Name LIMIT 5 OFFSET 10;
+
+-- count the number of rows
+SELECT COUNT(*) FROM Country WHERE Population > 10000 and Continent = 'Europe';
+
+-- only count the number of rows wher LifeExpectancy has data
+SELECT COUNT(LifeExpectancy) FROM Country;
+
+-- simple insert
+INSERT INTO customer (name, address, city, state, zip)
+    VALUES ('John Doe', '123 Sesame Street', 'Toronto', 'CA', '10055')
+
+-- simple update
+UPDATE customer SET address = '456 Sesame Street', zip = '90001' WHERE id = 5
+
+-- simple delete
+DELETE FROM customer WHERE id = 5;
+
 -- alter is used to add, delete or modify columns in an existing table
 -- is also to add and drop various constraints on an existing table
 ALTER TABLE table_name
