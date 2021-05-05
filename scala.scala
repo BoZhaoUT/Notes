fd5d6908fb5e9331b0eff9ad75e32a28d4729d94
Bo Zhao <bodotzhao@gmail.com>
2021-05-04 21:39:16 -0400

    update

--- scala.scala
+++ scala.scala
@@ -17,3 +17,8 @@ var x = 1 + 2 + 3.5     // Double, mixing double and int will get double
 List(1, 2, 3.5)         // List[Double], same as above
 List(1, true)           // List[AnyVal], mixing Int and Boolean will get AnyVal
 List(1, true, "peggy")  // List[any], mixing AnyVal and AnyRef will get Any
+
+
+// cons operator
+// the type is List[Int]. Nil is not part of the list, it indicates the end of a list
+val numbers = 1::2::3::4::Nil
